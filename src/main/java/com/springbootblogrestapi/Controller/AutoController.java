85d11accc299944a2b1c9c66c494d4eb932b6a59
Chiranjeev <chiranjeevj50@gmail.com>
2022-05-12 16:00:32 +0530

    Login/SignIn and Register/SignUp RESTAPI

--- src/main/java/com/springbootblogrestapi/Controller/AutoController.java
+++ src/main/java/com/springbootblogrestapi/Controller/AutoController.java
@@ -1,6 +1,11 @@
 package com.springbootblogrestapi.Controller;
 
+import com.springbootblogrestapi.Entity.Role;
+import com.springbootblogrestapi.Entity.User;
+import com.springbootblogrestapi.Repository.RoleRepository;
+import com.springbootblogrestapi.Repository.UserRepository;
 import com.springbootblogrestapi.payload.LoginDto;
+import com.springbootblogrestapi.payload.SignUpDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,10 +13,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.image.RescaleOp;
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -19,6 +28,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutoController {
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @PostMapping("/signin")
     public ResponseEntity<String>authenticateUser(@RequestBody LoginDto loginDto)
@@ -28,6 +46,41 @@ public class AutoController {
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return new ResponseEntity<>("USER SIGNED-IN SUCCESSFULLY", HttpStatus.OK);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<? > registerUser(@RequestBody SignUpDto signUpDto)
+    {
+        //add check for username exists in a DB
+        if(userRepository.existsByUsername(signUpDto.getUsername()))
+        {
+            return new ResponseEntity<>("Username is already taken!",HttpStatus.BAD_REQUEST);
+
+        }
+
+        //add check for email exists in a DB
+        if(userRepository.existsByEmail(signUpDto.getEmail()))
+        {
+            return new ResponseEntity<>("Email is already taken",HttpStatus.BAD_REQUEST);
+
+        }
+
+        //create user object
+
+        User user=new User();
+        user.setName(signUpDto.getName());
+        user.setUsername(signUpDto.getUsername());
+        user.setEmail(signUpDto.getEmail());
+        user.setPassword(passwordEncoder.encode(signUpDto.getPassword()));
+
+        Role roles=roleRepository.findByName("ROLE_ADMIN").get();
+        user.setRoles(Collections.singleton(roles));;
+
+
+        userRepository.save(user);
+
+        return new ResponseEntity<>("User registered successfully",HttpStatus.OK);
+
     }
 
 }
